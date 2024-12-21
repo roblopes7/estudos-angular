520fb858c2f45a114910fb2ef604f739b51b46af
roblopes7 <42593177+roblopes7@users.noreply.github.com>
2024-12-21 10:08:07 -0300

    Aula-components

---
+++ aula-1/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+    <h1>Curso de Angular</h1>
+    <app-new-component/>
+  `
+})
+export class AppComponent {
+  title = 'aula-1';
+}
